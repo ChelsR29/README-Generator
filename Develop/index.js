f13d15bae64a04413b1cf7479fc4b301cfa3d78d
Chelsea <chibby@chelseas-mbp.mynetworksettings.com>
2024-02-29 22:11:52 -0500

    Create README file function

--- Develop/index.js
+++ Develop/index.js
@@ -53,8 +53,12 @@ const questions = [
     }
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('README.md generated successfully!')
+    );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
