23e71b7bd6c470709b0e1c4cd4701e100c7fdde9
Chelsea <chibby@chelseas-mbp.mynetworksettings.com>
2024-02-29 22:17:14 -0500

    Render License Link

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -8,9 +8,15 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
+// Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+      return `https://opensource.org/licenses/${license}`;
+  } else {
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
